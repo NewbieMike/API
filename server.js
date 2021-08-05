31a57f4bd9a9f3ae0fd2d9674bd5d4db6b157067
Michał Micherda <mmicherdakr@gmail.com>
2021-08-05 13:45:56 +0200

    add page

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-
+const path = require('path');
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 
 const app = express();
-
+app.use(express.static(path.join(__dirname, '/client/build')));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
@@ -15,11 +15,15 @@ app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 // Przekierowanie w przypadku błędnego adresu strony
 app.use((req, res) => {
     res.status(404).send('404 not found...');
 });
 
-app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
+app.listen(process.env.PORT || 8000, () => {
+    console.log('Server is running on port: 8000');
 });
