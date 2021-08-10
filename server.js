77bd3fb356a713fdf1f74e4c88e77b046531f08a
Michał Micherda <mmicherdakr@gmail.com>
2021-08-10 07:24:49 +0200

    Add nodemon

--- server.js
+++ server.js
@@ -1,34 +1,40 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
- 
+const socket = require('socket.io');
+
+const app = express();
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 
-const app = express();
-
-app.use(express.static(path.join(__dirname, '/client/build/index.html')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build/index.html'));
-});
-
 app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/public/index.html'));
+});
 
 // Przekierowanie w przypadku błędnego adresu strony
 app.use((req, res) => {
     res.status(404).send('404 not found...');
 });
 
-app.listen(process.env.PORT || 8000, () => {
+const server = app.listen(process.env.PORT || 8000, () => {
     console.log('Server is running on port: 8000');
 });
+
+const io = socket(server);
+io.on('connection', (socket) => {
+    console.log('New user! Its id - ' + socket.id);
+    
+});
+
+//module.exports = server;
