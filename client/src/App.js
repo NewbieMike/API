a2f8c859d041c907742f2d353694c46656eb295a
Michał Micherda <mmicherdakr@gmail.com>
2021-08-10 07:39:16 +0200

    Add nodemon

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import io from 'socket.io-client';
+
 import MainLayout from './components/layout/MainLayout/MainLayout';
 
 // import routes
